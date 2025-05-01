c081366cb198754488e70a2b9eec716c71903dce
Samnang-Vouen <121634098+Samnang-Vouen@users.noreply.github.com>
2025-05-01 20:57:50 +0700

    Answer

---
+++ Start code/EX-3/model/RaceResult.js
@@ -0,0 +1,11 @@
+
+/**
+ * This class handles a single race time for a given participant and sport type.
+ */
+export class RaceResult {
+  constructor(participantId, sport, time) {
+    this.participant_id = participantId;
+    this.sport = sport;
+    this.time = time; // time is a Duration object
+  }
+}
